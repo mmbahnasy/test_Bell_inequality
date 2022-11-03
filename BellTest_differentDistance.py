3de95028c38b5ad01499ad923cf70f5b53f25f44
Mahmoud Bahnasy <mmbahnasy@gmail.com>
2022-11-02 23:36:45 -0400

    Bell test based at difference in distances
    Signed-off-by: Mahmoud Bahnasy <mmbahnasy@gmail.com>

--- BellTest_differentDistance.py
+++ BellTest_differentDistance.py
@@ -5,66 +5,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import withinRotatingRange, writeToCsvFile
 
-PI=180 # np.pi
+PI = 180  # np.pi
 ParticleNum = 1000
 RepeatitionNum = 100
 Theta_A = 0
 Theta_B = 60
 Theta_C = 120
 
-"""# A B !C + !A B C + A !B C >= 1"""
+
+"""# A B + B C + A C >= 1"""
+
 
 def getOneStateMeasurements(leftSideAngle, rightSideAngle):
-  "Returns A B !C, !A B C, A !B C"
-  stats = np.array([0,0,0])
-  if withinRotatingRange(leftSideAngle, Theta_A-PI/2, Theta_A+PI/2): # A
-    if withinRotatingRange(rightSideAngle, Theta_B-PI/2, Theta_B+PI/2): # B
-      stats[0]+=1
-    if withinRotatingRange(rightSideAngle, Theta_C-PI/2, Theta_C+PI/2): # C
-      stats[2]+=1
-  if withinRotatingRange(leftSideAngle, Theta_B-PI/2, Theta_B+PI/2): # B
-    if withinRotatingRange(rightSideAngle, Theta_C-PI/2, Theta_C+PI/2): # C
-      stats[1]+=1
-  return stats
+    "Returns A B ,  B C, A C"
+    isA_L = withinRotatingRange(leftSideAngle, Theta_A-PI/2+2*PI, Theta_A+PI/2)  # A
+    isA_R = withinRotatingRange(rightSideAngle, Theta_A-PI/2+2*PI, Theta_A+PI/2)  # A
+    isB_L = withinRotatingRange(leftSideAngle, Theta_B-PI/2+2*PI, Theta_B+PI/2)  # B
+    isB_R = withinRotatingRange(rightSideAngle, Theta_B-PI/2+2*PI, Theta_B+PI/2)  # B
+    isC_L = withinRotatingRange(leftSideAngle, Theta_C-PI/2, Theta_C+PI/2)  # C
+    isC_R = withinRotatingRange(rightSideAngle, Theta_C-PI/2, Theta_C+PI/2)  # C
+    # oneState = np.array([(isA_L and isB_R) or (not isA_L and not isB_R), (isB_L and isC_R) or (not isB_L and not isC_R), (isA_L and isC_R) or (not isA_L and not isC_R)])
+    oneState = np.array([isA_L and not isB_R, isB_L and not isC_R, isA_R and not isC_L])
+    # print(leftSideAngle, rightSideAngle, oneState)
+    return oneState
 
 # define statistic variables in a numpy array
 
-theta_a=0
-results=[]
-for i in range(RepeatitionNum):
-  particles = np.random.uniform(0,2*PI+1,ParticleNum)
-  distanceAngleShift = np.random.normal(0, 45, ParticleNum)
-  stats = np.array([0, 0, 0]) # Psame(A,B), Psame(B,C), Psame(A,C), 
-  for particleAngle in range(ParticleNum):
-    oneStat = getOneStateMeasurements(particles[i], (particles[i] + PI + distanceAngleShift[i]) % (2*PI))
-    stats = stats + oneStat
-  stats = stats/ParticleNum
-  # print(stats, np.sum(stats))
-  results.append(stats)
-  # pyplot.plot(result)
+def runTest(differentDistance):
+    """
+    Generate number of particls and perform the quantum entanglement test
+    Return the result as a list with three items: [N(a !b), N(b !c), N(a !c)]
+    """
+    particles = np.random.uniform(0, 2*PI, ParticleNum)
+    distanceAngleShift = np.random.normal(0, 2*PI, ParticleNum)
+    stats = np.array([0, 0, 0])  # Psame(A,B), Psame(B,C), Psame(A,C),
+    for indx in range(ParticleNum):
+        oneStat = getOneStateMeasurements(
+            particles[indx], (particles[indx] + distanceAngleShift[indx] if differentDistance else 0) % (2*PI))
+        stats = stats + oneStat
+    stats = stats / ParticleNum
 
-results = np.array(results)
-results_mean = np.mean(results, axis=0)
-print("Results (A B !C, !A B C, A !B C):", results_mean)
-results_sum = np.sum(results_mean)
-print(f"Results sum: {results_sum}") #, " not " if results_sum>= 1 else "", " violate Bell's inequality.")
-y_error =[np.std(results, axis=0)] 
+    return stats
 
-# Build the plot
-cases = ["A B !C", "!A B C", "A !B C"]
-x_pos = np.arange(len(cases))
-fig, ax = plt.subplots()
-ax.bar(x_pos, results_mean, yerr=y_error, align='center', alpha=0.5, ecolor='black', capsize=10)
-ax.set_ylabel('A B !C + !A B C + A !B C >= 1')
-ax.set_xticks(x_pos)
-ax.set_xticklabels(cases)
-ax.set_title('A B !C + !A B C + A !B C')
-ax.yaxis.grid(True)
+# results_sum = np.round(np.sum(results_mean), 2)
+# print(f"Results sum: {results_sum}")
+# y_error = [np.std(results, axis=0)]
 
-# Save the figure and show
-plt.tight_layout()
-plt.savefig('bar_plot_with_error_bars.png')
-plt.show()
 
-# write results into CSV file
-# writeToCsvFile('A B !C_!A B C_A !B C.csv', range(1, int(0.5*PI)), results_mean, y_errormin, y_errormax)
+
+# # write results into CSV file
+# # writeToCsvFile("A B _ B C_A C.csv", range(1, int(0.5*PI)), results_mean, y_errormin, y_errormax)
+
+
+if __name__ == "__main__":
+    distanceStatus = [False, True]
+    resultsAll =[]
+    for differentDistance in distanceStatus:
+        resultsPerRepetition =[]
+        for j in range(RepeatitionNum):
+            results = runTest(differentDistance = differentDistance) # dim: [N(a !b), N(b !c), N(a !c)]
+            resultsPerRepetition.append(results)
+        resultsAll.append(resultsPerRepetition)
+    resultsAll = np.array(resultsAll) # Dimension: 2 x RepetitionNum x 3
+    cases_results_mean = np.mean(resultsAll, axis=1) # Dimension: 2 x 3
+    for i, differentDistance in enumerate(distanceStatus):
+        isBellInequalityValid = cases_results_mean[i, 0] + cases_results_mean[i, 1] >= cases_results_mean[i, 2] # N(a !b) + N(b !c) >= N(a !c)
+        print("Different" if differentDistance else "Identical", "distance results (Sum/Count):",
+            cases_results_mean[i, 0] + cases_results_mean[i, 1], ">=", cases_results_mean[i, 2],
+            ", Bell's inequality (N(a !b) + N(b !c) >= N(a !c)) is", "" if isBellInequalityValid else "not", "valid.")
+    cases_results_min = np.min(resultsAll, axis=1)
+    cases_results_max = np.max(resultsAll, axis=1)
+    # Build the plot
+    cases = ["Identical (N(a !b) + N(b !c))", "Identical (N(a !c))", "Different (N(a !b) + N(b !c))", "Different (N(a !c))"]
+    x_pos = [0, 1, 3, 4]
+    cases_results = [cases_results_mean[0,0]+cases_results_mean[0,1], cases_results_mean[0,2],
+                    cases_results_mean[1,0]+cases_results_mean[1,1], cases_results_mean[1,2]]
+
+    y_errormin = [cases_results_mean[0,0]+cases_results_mean[0,1] - (cases_results_min[0,0]+cases_results_min[0,1]), cases_results_mean[0,1] - cases_results_min[0,0],
+                    cases_results_mean[1,0]+cases_results_mean[1,1]- (cases_results_min[1,0]+cases_results_min[1,1]), cases_results_mean[1,1] - cases_results_min[0,0]]
+    y_errormax = [(cases_results_max[0,0]+cases_results_max[0,1]) - (cases_results_mean[0,0]+cases_results_mean[0,1]),  cases_results_max[0,0] - cases_results_mean[0,1],
+                  (cases_results_max[1,0]+cases_results_max[1,1]) - (cases_results_mean[1,0]+cases_results_mean[1,1]),  cases_results_max[1,0] - cases_results_mean[1,1]]
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, cases_results, align="center", alpha=0.5, ecolor="black", capsize=10)
+    # ax.bar(x_pos, cases_results, yerr = [y_errormin, y_errormax], align="center", alpha=0.5, ecolor="black", capsize=10)
+    ax.set_ylabel("Correlation Probability")
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(cases)
+    ax.set_title("Correlation Probability")
+    ax.yaxis.grid(True)
+
+    # Save the figure and show
+    plt.tight_layout()
+    plt.savefig("CorrelationProbability.png")
+    # plt.show()
